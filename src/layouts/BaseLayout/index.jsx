bbfac74bfb8c95bdd49af4965e883fde1e9b53d5
CompaCodeRC <92763456+CompaCodeRC@users.noreply.github.com>
2022-10-04 23:31:32 -0500

    SEO, Border Box

--- src/layouts/BaseLayout/index.jsx
+++ src/layouts/BaseLayout/index.jsx
@@ -1,7 +1,19 @@
-export default function BaseLayout({ children }) {
+import Head from "next/head"
+
+export default function BaseLayout({ children, title }) {
     return (
-        <div>
-            {children}
-        </div>
+        <>
+            <Head>
+                <title>{process.env.NEXT_PUBLIC_TITLE}{title && ' - '+title}</title>
+                <meta name="description" content="Esto es una descripcion"/>
+                <meta name="keywords" content="Key, Word, Nextjs"/>
+                <meta name="og:title" property="og:title" content={`${process.env.NEXT_PUBLIC_TITLE}${title && ' - '+title}`} />
+                <meta name="og:description" property="og:description" content="Esto es una descripcion"/>
+                <meta name="og:image" property="og:image" content="#"/>
+            </Head>
+            <div>
+                {children}
+            </div>
+        </>
     )
 }
